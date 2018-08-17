f93efa23cd4fc518c526104969d0a71118020ce3
cgonz0 <cgonzadesign@gmail.com>
2018-08-17 09:31:19 -0700

    save playlist fix

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,7 +1,7 @@
 let accessToken;
 let expiresIn;
-const redirectURI = 'http://jammmies.surge.sh';
-// const redirectURI = 'http://localhost:3000/';
+// const redirectURI = 'http://jammmies.surge.sh';
+const redirectURI = 'http://localhost:3000/';
 const clientID = '08d71502090b47049162490f1d678ee7';
 
 const Spotify = {
@@ -60,10 +60,8 @@ const Spotify = {
     const headers = {
       Authorization: `Bearer ${accessToken}`
     }
-    // let userID;
-    // let playListID;
 
-    if (playListName && trackURIs) {
+    if (!playListName && !trackURIs.length) {
       return;
     }
     // Make a request to retrieve user ID
